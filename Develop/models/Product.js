c1a85d881b60a0fd10c9612b2c6862f44c874f64
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-07 13:02:41 -0800

    added/remove comments

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,15 +1,53 @@
-// import important parts of sequelize library
+
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+
 Product.init(
   {
     // define columns
+    id: {
+      // defining the type for the model
+      type: DataTypes.INTEGER,
+      // Checks whether or not allows null
+      allowNull: false,
+      // Allows for unique identifer
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // Checks if the value is true
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Sets value at 10
+      defaultValue: 10,
+      validate: {
+        isDecimal: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      // refers to catergory model id
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
